9a0249e3b5ee7350ee7716b84d15907babde96c4
seokbyeong chae <sbchaes@gmail.com>
2020-12-14 00:31:43 +0900

    First upload

---
+++ frontend/pages/login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input v-model="email" />
+    <input v-model="password" />
+    <button @click="onClickLogIn">LogIn</button>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    ...mapActions('account', ['logIn']),
+    onClickLogIn() {
+      this.logIn([this.email, this.password])
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ frontend/store/account.js
@@ -0,0 +1,9 @@
+export const state = () => ({})
+
+export const mutations = {}
+
+export const actions = {
+  logIn({ commit }, [email, password]) {
+    console.log(email, password)
+  },
+}
