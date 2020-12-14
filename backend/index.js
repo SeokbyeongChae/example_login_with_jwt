5bef799d4d3b73b1fb5d82ea700a02e79cc1cd72
seokbyeong chae <sbchaes@gmail.com>
2020-12-15 00:03:10 +0900

    First upload of server code

---
+++ backend/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+const port = 4001;
+
+const privateKey = "test123";
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.post("/login", (req, res) => {
+  const token = jwt.sign({ foo: "bar" }, privateKey, { expiresIn: 15 });
+  console.dir(token);
+
+  res.cookie("sid", token);
+  console.log(req.cookies);
+  /**
+	 * {options}
+	 	- maxAge : 현재 시간으로부터 만료 시간을 밀리초 단위로 설정
+		- expires : cookie의 만료날짜를 GMT시간으로 설정
+		- path : cookie의 경로, default는 "/"
+		- domain : cookie의 domain name, default는 loaded
+		- secure : https에서만 cookie를 사용할 수 있게 설정
+		- httpOnly : 웹 서버를 통해서만 cookie에 접근할 수 있도록 설정
+		- signed : cookie가 서명되어야 할 지를 결정
+	 */
+
+	 // res.clearCookie(key); 쿠키삭제
+  res.send(token);
+});
+
+app.post("/authorization", (req, res) => {
+  // console.log(req.body.token);
+  console.dir(jwt.verify(req.body.token, privateKey));
+
+  res.send("welcome, " + req.body.token);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
